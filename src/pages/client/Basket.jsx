ca9abe7932800557e6481ea363f9f7a30acb22d6
El Ayachi Abdelmajid <elayachiabdel2001@gmail.com>
2022-06-10 13:23:16 +0100

    responsive

--- src/pages/client/Basket.jsx
+++ src/pages/client/Basket.jsx
@@ -35,7 +35,7 @@ function Basket() {
       )}
       <div className="max-w-screen-xl m-auto">
         <Nav />
-        <CheckoutNav active='card'/>
+        <CheckoutNav active="card" />
         <div className="mx-auto w-10/12">
           <div className="flex justify-between items-center">
             <h1 className="text-2xl font-semibold text-gray-800 mx-1 my-4">
@@ -49,18 +49,18 @@ function Basket() {
               Clear Card
             </span>
           </div>
-          <div className="w-full flex bg-gray-100 p-2 border-b border-gray-300">
+          <div className="w-full flex bg-gray-100 p-2">
             <div className="text-lg w-6/12 font-medium">Articles</div>
-            <div className="text-lg w-2/12 font-normal flex justify-center">
+            <div className="text-lg w-2/12 font-normal md:flex hidden justify-center ">
               Quantity
             </div>
-            <div className="text-lg w-2/12 font-normal flex justify-center">
+            <div className="text-lg w-2/12 font-normal md:flex hidden justify-center">
               Price Item
             </div>
-            <div className="text-lg w-2/12 font-normal flex justify-center">
+            <div className="text-lg w-2/12 font-normal md:flex hidden justify-center">
               Total Price
             </div>
-            <div className="text-lg w-2/12 font-normal flex justify-center">
+            <div className="text-lg w-2/12 font-normal md:flex hidden justify-center">
               Delete
             </div>
           </div>
@@ -70,9 +70,9 @@ function Basket() {
               return (
                 <div
                   key={index}
-                  className="w-full flex py-2 border-b border-gray-300"
+                  className="w-full flex md:flex-row flex-col my-5 py-2 border-b border-gray-300"
                 >
-                  <div className="w-6/12 flex">
+                    <div className="md:w-6/12 w-full  flex md:flex-row flex-row-reverse">
                     <div className="w-1/3 pr-4">
                       <img
                         src={require(`../../assets/images/uploads/${product.product.images[0].path}`)}
@@ -91,7 +91,7 @@ function Basket() {
                       </span>
                     </div>
                   </div>
-                  <div className="text-lg w-2/12 font-normal flex flex-col items-center justify-center">
+                  <div className="text-lg w-2/12 font-normal md:flex hidden flex-col items-center justify-center">
                     <div className="flex  rounded-sm h-7 w-24">
                       <button
                         onClick={() => {
@@ -132,16 +132,108 @@ function Basket() {
                     </div>
                   </div>
 
-                  <div className="text-lg w-2/12 font-normal md:flex items-center justify-center hidden">
-                    {product.product.price}
+                  <div className="w-full md:hidden flex flex-row">
+                    <div className="text-lg w-2/12 font-normal flex  flex-col items-center justify-center">
+                      <div className="flex  rounded-sm h-7  w-24">
+                        <button
+                          onClick={() => {
+                            if (product.quantity < 20) {
+                              dispatch({
+                                type: "INCREMENT_FROM_CART",
+                                payload: product.product.id,
+                              });
+                              setErrorInput(false);
+                            } else {
+                              setErrorInput(true);
+                            }
+                          }}
+                          className="w-7 h-7 flex items-center justify-center border-2 border-gray-500 rounded-full font-extrabold"
+                        >
+                          +
+                        </button>
+                        <span className="text-xl font-normal text-gray-800 w-6 flex justify-center">
+                          {product.quantity}
+                        </span>
+
+                        <button
+                          onClick={() => {
+                            if (product.quantity > 1) {
+                              dispatch({
+                                type: "DECREMENT_FROM_CART",
+                                payload: product.product.id,
+                              });
+                              setErrorInput(false);
+                            } else {
+                              setErrorInput(true);
+                            }
+                          }}
+                          className="w-7 h-7 flex items-center justify-center border-2 border-gray-500 rounded-full font-extrabold"
+                        >
+                          -
+                        </button>
+                      </div>
+                    </div>
+                    <div className="flex justify-around w-full my-4">
+                      <div className="text-lg md:w-2/12 w-full font-normal flex  items-center justify-center ">
+                        {product.product.price}
+                      </div>
+                      <div className="text-lg md:w-2/12 w-full font-normal flex  items-center justify-center ">
+                        {product.product.price * product.quantity}
+                      </div>
+                    </div>
+                    <div className="text-lg w-2/12 font-normal md:flex hidden flex-col items-center justify-center">
+                      <div className="flex  rounded-sm h-7 w-24">
+                        <button
+                          onClick={() => {
+                            if (product.quantity < 20) {
+                              dispatch({
+                                type: "INCREMENT_FROM_CART",
+                                payload: product.product.id,
+                              });
+                              setErrorInput(false);
+                            } else {
+                              setErrorInput(true);
+                            }
+                          }}
+                          className="w-7 h-7 flex items-center justify-center border-2 border-gray-500 rounded-full font-extrabold"
+                        >
+                          +
+                        </button>
+                        <span className="text-xl font-normal text-gray-800 w-6 flex justify-center">
+                          {product.quantity}
+                        </span>
+
+                        <button
+                          onClick={() => {
+                            if (product.quantity > 1) {
+                              dispatch({
+                                type: "DECREMENT_FROM_CART",
+                                payload: product.product.id,
+                              });
+                              setErrorInput(false);
+                            } else {
+                              setErrorInput(true);
+                            }
+                          }}
+                          className="w-7 h-7 flex items-center justify-center border-2 border-gray-500 rounded-full font-extrabold"
+                        >
+                          -
+                        </button>
+                      </div>
+                    </div>
+
+                    <div className="text-lg md:w-2/12 w-full font-normal md:flex hidden items-center justify-center ">
+                      {product.product.price}
+                    </div>
                   </div>
-                  <div className="text-lg w-2/12 font-normal md:flex items-center justify-center hidden">
+
+                  <div className="text-lg md:w-2/12 w-full font-normal md:flex hidden items-center justify-center ">
                     {product.product.price * product.quantity}
                   </div>
                   <button
                     type="button"
                     onClick={() => removeItemHandler(product.product.id)}
-                    className="text-lg w-2/12 font-normal flex  items-center justify-center"
+                    className="text-lg md:w-2/12 w-full font-normal flex  items-center justify-center"
                   >
                     <RiDeleteBin6Line />
                   </button>
@@ -156,17 +248,33 @@ function Basket() {
             </div>
           )}
           {products.length > 0 && (
-              <div className="w-full flex md:flex-row flex-col justify-end items-center border-b border-gray-300 gap-3">
-                <span className="text-lg font-semibold text-gray-600 mx-2 py-4 "> You choose <span className="font-bold text-gray-900">{products.length}</span>  items</span>
-                <span className="text-xl font-semibold text-gray-600 mx-2 my-4">
-                  Total:&nbsp;
-                  <span className="text-red-500 text-2xl">$
+            <div className="w-full flex md:flex-row flex-col justify-end items-center gap-3">
+              <span className="text-lg font-semibold text-gray-600 mx-2 py-4 ">
+                {" "}
+                You choose{" "}
+                <span className="font-bold text-gray-900">
+                  {products.length}
+                </span>{" "}
+                items
+              </span>
+              <span className="text-xl font-semibold text-gray-600 mx-2 my-4">
+                Total:&nbsp;
+                <span className="text-red-500 text-2xl">
+                  $
                   {products.reduce(
                     (acc, item) => acc + item.product.price * item.quantity,
                     0
-                  )}</span>
+                  )}
                 </span>
-                <span className=""><Link to="/place-order" className="py-2 px-4 text-xl font-semibold text-gray-100 bg-blue-600 rounded">Proceed to checkout</Link></span>
+              </span>
+              <span className="">
+                <Link
+                  to="/place-order"
+                  className="py-2 px-4 text-xl font-semibold text-gray-100 bg-blue-600 rounded"
+                >
+                  Proceed to checkout
+                </Link>
+              </span>
             </div>
           )}
         </div>
